0bbc766f6e75267f39a06995fb90091d8f934bd5
Ajay <ajay.thirumalesh@gmail.com>
2024-07-08 08:04:36 +0530

    first commit

---
+++ home_screen/Testing.java
@@ -0,0 +1,10 @@
+package home_screen;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("Hello this is priya and this is my first project");
+	}
+
+}
